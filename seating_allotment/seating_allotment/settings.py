bdb0a862bc4280f352e4119b4b1149873a3933d5
kesavanselva50-lang <kesavanselva50@gmail.com>
2026-01-22 18:23:45 +0530

    change to postgre

--- seating_allotment/seating_allotment/settings.py
+++ seating_allotment/seating_allotment/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'seating_allotment.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Seating_db',
+        'USER': 'postgres',
+        'PASSWORD': '1203',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
