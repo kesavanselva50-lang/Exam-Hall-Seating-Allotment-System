b0b7d3cab85fd9e8aa844453ff198c5043858291
kesavanselva50-lang <kesavanselva50@gmail.com>
2026-01-27 13:56:47 +0530

    enhanced

--- seating_allotment/allotment/admin.py
+++ seating_allotment/allotment/admin.py
@@ -7,9 +7,39 @@ from .models import (
     ExamHall,
     SeatingAllotment
 )
-admin.site.register(Department)
-admin.site.register(Student)
-admin.site.register(Subject)
-admin.site.register(Exam)
-admin.site.register(ExamHall)
-admin.site.register(SeatingAllotment)
+
+@admin.register(Department)
+class DepartmentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('reg_no', 'name', 'department', 'semester')
+    list_filter = ('department', 'semester')
+    search_fields = ('reg_no', 'name')
+
+
+@admin.register(Subject)
+class SubjectAdmin(admin.ModelAdmin):
+    list_display = ('subject_code', 'subject_name', 'department', 'semester')
+    list_filter = ('department', 'semester')
+
+
+@admin.register(Exam)
+class ExamAdmin(admin.ModelAdmin):
+    list_display = ('subject', 'exam_date', 'session')
+    list_filter = ('exam_date', 'session')
+
+
+@admin.register(ExamHall)
+class ExamHallAdmin(admin.ModelAdmin):
+    list_display = ('hall_no', 'total_seats')
+    search_fields = ('hall_no',)
+
+
+@admin.register(SeatingAllotment)
+class SeatingAllotmentAdmin(admin.ModelAdmin):
+    list_display = ('exam', 'student', 'hall', 'seat_number')
+    list_filter = ('exam', 'hall')
+    search_fields = ('student__reg_no',)
--- seating_allotment/seating_allotment/settings.py
+++ seating_allotment/seating_allotment/settings.py
@@ -38,7 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'allotment',
+    'tailwind',
+    'theme',
 ]
+TAILWIND_APP_NAME = 'theme'
+NPM_BIN_PATH = r"C:\Program Files\nodejs\npm.cmd"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
